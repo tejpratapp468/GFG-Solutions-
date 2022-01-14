50b92af15df6e05fde38588becdfbd95a4e3e364
tejpratapp468 <58838640+tejpratapp468@users.noreply.github.com>
2022-01-14 14:24:55 +0530

    Update Add all greater values to every node in a BST.cpp

--- Add all greater values to every node in a BST.cpp
+++ Add all greater values to every node in a BST.cpp
@@ -1,5 +1,27 @@
 //Time Complexity O(n) n=number of nodes in bst
 //Space Complexity O(h) , h=heightoftree, best case(balanced bst) h=logn, worst case(skewed bst)h=n
+
+//method 1: passing address of greaterSum address in helper function
+
+void helper(Node* root, int& greaterSum){
+    if(root==NULL) return;
+    helper(root->right,greaterSum);
+    root->data+=greaterSum;
+    greaterSum=root->data;
+    helper(root->left,greaterSum);
+}
+// modify the BST and return its root
+Node* modify(Node *root)
+{
+    // Your code here
+    int greaterSum=0;
+    helper(root,greaterSum);
+    return root;
+}
+
+
+
+//method 2: taking greaterSumas global variable
 int greaterSum=0;
 void helper(Node* root){
     if(root==NULL) return;
@@ -12,7 +34,7 @@ void helper(Node* root){
 Node* modify(Node *root)
 {
     // Your code here
-    //int greaterSum=0;
+    greaterSum=0;
     helper(root);
     return root;
 }
